6d2b19a8457f6779b8d87ecc24ba592403a23a61
jorddo <jordanbsneed@gmail.com>
2022-06-06 17:13:37 -0500

    form updates

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,28 +1,49 @@
 import { TextField, Button } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 
 function ContactForm() {
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+  // const [formState, setFormState] = useState({
+  //   name: '',
+  //   email: '',
+  //   message: '',
+  // });
 
-  const { name, email, message } = formState;
+  // const { name, email, message } = formState;
 
-  function handleChange(e) {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
-  }
+  // function handleChange(e) {
+  //   setFormState({ ...formState, [e.target.name]: e.target.value });
+  // }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
-  }
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   console.log(formState);
+  // }
 
   return (
     <section>
       <h1>Contact Me</h1>
-      <form id='contact-form' onSubmit={handleSubmit}>
+      <form name='contact' method='post'>
+        <input type='hidden' name='form-name' value='contact' />
+        <p>
+          <label>
+            Your Name: <input type='text' name='name' />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type='email' name='email' />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name='message'></textarea>
+          </label>
+        </p>
+        <p>
+          <button type='submit'>Send</button>
+        </p>
+      </form>
+      {/* <form id='contact-form' onSubmit={handleSubmit}>
         <input type='hidden' name='form-name' value='contact' />
         <TextField
           label='Name'
@@ -53,10 +74,8 @@ function ContactForm() {
           onChange={handleChange}
           name='message'
         />
-        <Button variant='contained' type='submit'>
-          Submit
-        </Button>
-      </form>
+        <Button variant='contained'>Submit</Button>
+      </form> */}
     </section>
   );
 }
