d2e14de98f37cdb55f4f0efb326151f07595c945
jorddo <jordanbsneed@gmail.com>
2022-06-07 15:51:07 -0500

    updating about me

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,9 +4,9 @@ const AboutPage = () => (
   <Section id='about'>
     <h2>About Page</h2>
     <p>
-      My name is Jordan, and I'm a web developer. I am married with a kid on the
-      way. I have two cats named Mavis and Townes, and I've been living in
-      Austin for about 25 years.
+      My name is Jordan, and I'm a Full-Stack Web Developer. I am married with a
+      daughter on the way. I have two cats named Mavis and Townes, and I've been
+      living in Austin for about 25 years.
     </p>
   </Section>
 );
