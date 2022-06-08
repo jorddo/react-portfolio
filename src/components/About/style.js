4a03629afbe98d889988d7b6f34a3810a168845b
jorddo <jordanbsneed@gmail.com>
2022-06-08 15:30:57 -0500

    changing import in style.js

--- src/components/About/style.js
+++ src/components/About/style.js
@@ -1,5 +1,5 @@
 import { styled } from '@mui/system';
-import { Container } from '@mui/system';
+import { Container } from '@mui/material';
 
 const StyledAbout = styled(Container)`
   display: flex;
